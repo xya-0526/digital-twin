284018d7d39d88bc5b6d27e5fd896cac53fb6394
leionz <2984353371@qq.com>
2025-05-08 21:06:08 +0800

    api and Element finish

--- src/main.js
+++ src/main.js
@@ -2,11 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import route from './routers'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
-app.use(ElementPlus)
 app.use(route)
 app.mount('#app')
