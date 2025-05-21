0bfa64d3b1bbbd77fb7096a946e415a56b2eb37e
minorcell <minorcell6789@gmail.com>
2025-05-21 13:58:42 +0800

    style: format SelectBox component CSS with consistent indentation and spacing

--- src/components/SeclectBox.vue
+++ src/components/SeclectBox.vue
@@ -20,61 +20,62 @@ import UniversalButton from './UniversalButton.vue'
 
 const selectArray = ['显示行政区划', '2413', '660', '主要交通路网', '36.6边界', '主要设施用地']
 </script>
+
+<style scoped>
 .checkBox {
-    border: 0.1rem solid #456655;
-    border-radius: 0.8rem;
-    padding-top: 2.6rem;
-    color: rgb(202, 200, 200);
-    width: 25.9rem;
-    height: 37.7rem;
-    background: #142222;
-    right: 130rem;
-    top: 85rem;
-    position: absolute;
+  border: 0.1rem solid #456655;
+  border-radius: 0.8rem;
+  padding-top: 2.6rem;
+  color: rgb(202, 200, 200);
+  width: 25.9rem;
+  height: 37.7rem;
+  background: #142222;
+  right: 130rem;
+  top: 85rem;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  #box {
+    line-height: 2.4rem;
+
+    span {
+      height: 2.4rem;
+      text-align: center;
+      line-height: 2.4rem;
+    }
+
+    font-size: 2.4rem;
     display: flex;
-    flex-direction: column;
+    width: 100%;
+    height: 2.4rem;
+  }
+
+  #box :last-child {
+    margin-bottom: 3.38rem;
+  }
+
+  input[type='checkbox'] {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    margin-right: 1rem;
+    margin-left: 3.6rem;
+    width: 2.4rem;
+    height: 2.4rem;
+    background-color: transparent;
+    border: #58896cff 0.1rem solid;
+  }
+
+  input[type='checkbox']:checked:before {
+    display: flex;
+    justify-content: center;
     align-items: center;
-    justify-content: space-between;
-
-    #box {
-        line-height: 2.4rem;
-
-        span {
-            height: 2.4rem;
-            text-align: center;
-            line-height: 2.4rem;
-        }
-
-        font-size:2.4rem;
-        display: flex;
-        width: 100%;
-        height: 2.4rem;
-    }
-
-    #box :last-child {
-        margin-bottom: 3.38rem;
-    }
-
-    input[type="checkbox"] {
-        -webkit-appearance: none;
-        -moz-appearance: none;
-        appearance: none;
-        margin-right: 1rem;
-        margin-left: 3.6rem;
-        width: 2.4rem;
-        height: 2.4rem;
-        background-color: transparent;
-        border: #58896CFF 0.1rem solid;
-    }
-
-    input[type="checkbox"]:checked:before {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        content: '\2713';
-        color: white;
-        font-size: 1.4rem;
-    }
-
+    content: '\2713';
+    color: white;
+    font-size: 1.4rem;
+  }
 }
 </style>
